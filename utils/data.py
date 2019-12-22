6eddc72db7c0f0be40e3944f5aa78e45be482b27
victorchen_lab <victor_tochen@foxmail.com>
2019-12-22 15:06:50 +0800

    add retrived version of visulization and celebA dataset

--- utils/data.py
+++ utils/data.py
@@ -7,6 +7,9 @@
         load serveral dataset， 加载数据集
 """
 
+import os,sys
+import PIL.Image as Image
+
 from torch.utils.data import DataLoader, Dataset
 from torchvision.datasets import MNIST, CIFAR10, ImageFolder
 import torchvision.transforms as transforms
@@ -65,17 +68,81 @@ def load_Anime_faces():
 
     root = baseroot + "animeface-character-dataset/"
 
+    means = [0.5, 0.5, 0.5]
+    stds = [0.5, 0.5, 0.5]
+
     dataset = ImageFolder(root + "thumb/", transform=transforms.Compose([
         transforms.CenterCrop(120),
         transforms.Resize(64),
         transforms.ToTensor(),
-        transforms.Normalize([0.5, 0.5, 0.5],[0.5, 0.5, 0.5])
+        transforms.Normalize(means, stds)
+    ]))
+
+    setattr(dataset, "mean", means)
+    setattr(dataset, "std", stds)
+
+    loader_params = {
+        "num_workers": 2,
+        "shuffle": True,
+        "batch_size": 128
+    } 
+
+    train = DataLoader(dataset, **loader_params)
+
+    return train
+
+
+class CelebA(Dataset):
+    """ CelebA Dataset, using the aligned and croped version.
+        http://mmlab.ie.cuhk.edu.hk/projects/CelebA.html
+    """
+    def __init__(self, root = "/home/victorchen/workspace/Venus/celebA/", transform = None , target_transform = None):
+
+        self.img_dir = root + "images/"
+        identity_file = root + "Anno/identity_CelebA.txt"
+
+        with open(identity_file, "r") as f:
+            self.identitiy = f.readlines()
+
+        self.transform = transform
+        self.target_transform = target_transform
+        self.channel_mean = [0.50003925, 0.42008925, 0.37377534]
+        self.channel_std = [0.30878809, 0.28794379, 0.28661432]
+
+    def __len__(self):
+        return len(self.identitiy)
+
+    def __getitem__(self, idx):
+
+        image, label = self.identitiy[idx].replace("\r","").replace("\n","").split()
+
+        image = Image.open(self.img_dir + image)
+
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+
+        return image, label
+        
+        
+def load_celebA():
+    """ default load celebA dataset
+    """
+    channel_mean = [0.50003925, 0.42008925, 0.37377534]
+    channel_std = [0.30878809, 0.28794379, 0.28661432]
+
+    dataset = CelebA(transform=transforms.Compose([
+        transforms.CenterCrop(178),
+        transforms.Resize(64),
+        transforms.ToTensor(),
+        transforms.Normalize(channel_mean, channel_std)
     ]))
 
     loader_params = {
         "num_workers": 2,
         "shuffle": True,
-        "batch_size": 32
+        "batch_size": 128
     } 
 
     train = DataLoader(dataset, **loader_params)
--- utils/visulize.py
+++ utils/visulize.py
@@ -16,7 +16,27 @@ import torch
 from torchvision.utils import make_grid, save_image
 
 def show_batch_images(batches, figsize = (15,15), title="images"):
-    images = make_grid(batches).detach().numpy().transpose((1,2,0))
+    images = make_grid(batches, nrow=16).detach().cpu().numpy().transpose((1,2,0))
+    
+    figure = plt.figure(figsize=figsize)
+    plt.imshow(images)
+    plt.xticks([])
+    plt.yticks([])
+    plt.title(title)
+    return plt
+
+
+def show_retrived_batch_images(batches, means, stds, figsize = (15,15), title="images"):
+
+    batch_size, num_channel = batches.size()[:2]
+    nrow = int(batch_size ** 0.5) 
+
+    images = make_grid(batches, nrow).detach().cpu().numpy().transpose((1,2,0))
+
+    for channel in range(num_channel):
+        images[:,:,channel] = ( images[:,:,channel] * stds[channel] + means[channel] ) * 255.0
+        
+    images = images.astype(int)
     
     figure = plt.figure(figsize=figsize)
     plt.imshow(images)
@@ -26,4 +46,18 @@ def show_batch_images(batches, figsize = (15,15), title="images"):
     return plt
 
 def save_batch_images(batches, image_name, root = "./saved/images/"):
-    save_image(batches, "{}{}.png".format(root, image_name))
+    save_image(batches, "{}{}.png".format(root, image_name), nrow=16)
+
+def save_retrived_batch_images(batches, means, stds, image_name, root = "./saved/images/"):
+
+    batch_size, num_channel = batches.size()[:2]
+    nrow = int(batch_size ** 0.5) 
+
+    with torch.no_grad():
+        for channel in range(num_channel):
+            batches[:,:,channel] = (batches[:,:,channel] * stds[channel]
+                 + means[channel]) * 255.0
+    
+        batches = torch.as_tensor(batches, dtype=torch.int)
+
+    save_image(batches, "{}{}.png".format(root, image_name), nrow)
